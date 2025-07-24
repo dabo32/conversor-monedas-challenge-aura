0baeffef23e4b213388252e3eb096e3a4f3dfa3f
David Santiago Bohorquez Gonzalez <dasabo30@gmail.com>
2025-07-24 08:22:47 -0500

    "feat: Configurar Main como punto de entrada de la aplicación" -m "Main ahora solo inicia ConversorDeMonedasApp, delegando la lógica principal."

---
+++ src/main/java/com/alura/conversorMonedas/Main.java
@@ -0,0 +1,10 @@
+package com.alura.conversorMonedas;
+
+public class Main {
+    public static void main(String[] args) {
+        //Crea una instancia de la aplicación del conversor
+        ConversorDeMonedasApp app = new ConversorDeMonedasApp();
+        //Ejecuta la lógica principal de la aplicación
+        app.ejecutar();
+    }
+}
